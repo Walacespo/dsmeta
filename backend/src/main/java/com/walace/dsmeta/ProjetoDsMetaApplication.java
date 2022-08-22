18bb65846346a00dd89475d1f963806424d0df7d
Walace da Silva <walacespo@gmail.com>
2022-08-22 08:42:05 -0300

    Project created

---
+++ backend/src/main/java/com/walace/dsmeta/ProjetoDsMetaApplication.java
@@ -0,0 +1,13 @@
+package com.walace.dsmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDsMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDsMetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/walace/dsmeta/ProjetoDsMetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walace.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDsMetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
